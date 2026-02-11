e738bd4102255857ab9df6dff630ca5d4e64aa5c
mihirxtc <mihirxtc@gmail.com>
2026-02-11 12:58:06 +0000

    Dockerized application

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Home Page")
+    res.send("Welcome to homepage!")
 });
 
 app.get("/about", (req, res) => {
-    res.send("About Page")
+    res.send("Welcome to about page!")
 });
 
 app.get("/contact", (req, res) => {
-    res.send("Contact Page")
+    res.send("Welcome to contact page!")
 });
 
 app.get("/devops", (req, res) => {
-    res.send("End goal is learning not beautify it")
+    res.send("Welcome to DevOps page! trying my best to scale-up 🚀")
 });
 
 app.listen(port, () => {
